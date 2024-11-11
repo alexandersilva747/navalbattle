a11e32fb9d73e8ca452276b34780c9d732a59492
Sebastian Orejuela <sebastian.albornoz@correounivalle.edu.co>
2024-11-11 01:16:53 -0500

    adición de botones al tablero de la maquina y sus onAction names

--- src/main/java/org/example/navalbattle/controllers/GameController.java
+++ src/main/java/org/example/navalbattle/controllers/GameController.java
@@ -2,7 +2,11 @@ package org.example.navalbattle.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 import org.example.navalbattle.model.Player;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class GameController {
     //control entre logica y vista
@@ -10,6 +14,29 @@ public class GameController {
 
     @FXML
     private Label nicknameLabel;
+
+    @FXML
+    private Rectangle aircraft, destroyer, destroyer2, destroyer3, frigate, frigate2, frigate3, frigate4,submarine, submarine2;
+
+    Draggable draggable=new Draggable();
+
+
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        draggable.makeDraggable(aircraft);
+        draggable.makeDraggable(destroyer);
+        draggable.makeDraggable(destroyer2);
+        draggable.makeDraggable(destroyer3);
+        draggable.makeDraggable(frigate);
+        draggable.makeDraggable(frigate2);
+        draggable.makeDraggable(frigate3);
+        draggable.makeDraggable(frigate4);
+        draggable.makeDraggable(submarine);
+        draggable.makeDraggable(submarine2);
+    }
+
+
+
 
 
     public void setPlayer(Player player) {
