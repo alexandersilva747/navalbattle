c498591a75daa5acb1bee125e354d62c330a7b8c
Lady Matabanchoy <lady.matabanchoy@correounivalle.edu.co>
2024-11-14 00:47:03 -0500

    Añadiendo toda la clase de figuras con sus respectivas imagenes, tambien añadiendolas en el tablero y asi mismo, creando la clase TableroAliado, con su funcionamiento en arrastre y limitando la flota del usuario

--- src/main/java/org/example/navalbattle/controllers/GameController.java
+++ src/main/java/org/example/navalbattle/controllers/GameController.java
@@ -1,543 +1,68 @@
 package org.example.navalbattle.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.input.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import org.example.navalbattle.model.TableroAliado;
 import org.example.navalbattle.model.Player;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import org.example.navalbattle.model.figures;
 
 public class GameController {
-    //control entre logica y vista
     private Player player;
 
     @FXML
     private Label nicknameLabel;
 
     @FXML
-    private Rectangle aircraft, destroyer, destroyer2, destroyer3, frigate, frigate2, frigate3, frigate4,submarine, submarine2;
+    private Pane contenedorBarco1, contenedorSubmarino1, contenedorAircraft1,contenedorDestroyer1;
 
-    @FXML
-    void buttonBoardMachine1(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine10(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine100(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine11(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine12(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine13(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine14(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine15(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine16(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine17(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine18(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine19(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine2(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine20(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine21(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine22(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine23(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine24(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine25(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine26(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine27(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine28(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine29(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine3(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine30(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine31(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine32(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine33(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine34(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine35(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine36(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine37(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine38(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine39(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine4(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine40(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine41(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine42(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine43(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine44(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine45(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine46(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine47(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine48(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine49(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine5(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine50(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine51(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine52(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine53(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine54(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine55(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine56(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine57(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine58(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine59(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine6(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine60(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine61(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine62(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine63(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine64(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine65(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine66(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine67(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine68(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine69(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine7(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine70(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine71(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine72(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine73(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine74(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine75(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine76(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine77(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine78(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine79(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine8(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine80(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine81(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine82(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine83(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine84(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine85(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine86(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine87(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine88(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine89(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine9(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buttonBoardMachine90(ActionEvent event) {
-
-    }
 
-    @FXML
-    void buttonBoardMachine91(ActionEvent event) {
-
-    }
 
     @FXML
-    void buttonBoardMachine92(ActionEvent event) {
+    private GridPane boardGridPane;
 
-    }
+    private TableroAliado tableroAliado;
 
-    @FXML
-    void buttonBoardMachine93(ActionEvent event) {
 
-    }
 
-    @FXML
-    void buttonBoardMachine94(ActionEvent event) {
+    public void initialize() {
+        // Inicializar una fragata
+        figures barco = new figures(150, 40, "fragata"); //Creación de una fragata en Pane, visualización en juego
+        contenedorBarco1.getChildren().add(barco);
 
-    }
+        // Inicializar un submarino
+        figures submarino = new figures(150, 40, "submarino"); //Creación submarino, visualizacion juego
+        contenedorSubmarino1.getChildren().add(submarino);
 
-    @FXML
-    void buttonBoardMachine95(ActionEvent event) {
+        // Inicializar un avión
+        figures avion = new figures(150, 40, "aircraft"); //Creación portaaviones, visual
+        contenedorAircraft1.getChildren().add(avion);
 
-    }
+        // Inicializar un destructor
+        figures destructor = new figures(150, 40, "destroyer"); //Creación destructor, visual
+        contenedorDestroyer1.getChildren().add(destructor);
 
-    @FXML
-    void buttonBoardMachine96(ActionEvent event) {
+        tableroAliado = new TableroAliado(contenedorBarco1, contenedorSubmarino1, contenedorAircraft1, contenedorDestroyer1, boardGridPane);
 
+        //se mandan los objetos al tablero del aliado
     }
 
     @FXML
-    void buttonBoardMachine97(ActionEvent event) {
-
+    public void handleDragDetected(MouseEvent event) { //Eventos para detectar arrastre
+        tableroAliado.handleDragDetected(event);
     }
 
     @FXML
-    void buttonBoardMachine98(ActionEvent event) {
-
+    public void handleDragOver(DragEvent event) { //Evento para detectar donde se pone el objeto
+        tableroAliado.handleDragOver(event);
     }
 
     @FXML
-    void buttonBoardMachine99(ActionEvent event) {
-
-    }
-    Draggable draggable=new Draggable();
-
-
-    public void initialize(URL url, ResourceBundle rb)
-    {
-        draggable.makeDraggable(aircraft);
-        draggable.makeDraggable(destroyer);
-        draggable.makeDraggable(destroyer2);
-        draggable.makeDraggable(destroyer3);
-        draggable.makeDraggable(frigate);
-        draggable.makeDraggable(frigate2);
-        draggable.makeDraggable(frigate3);
-        draggable.makeDraggable(frigate4);
-        draggable.makeDraggable(submarine);
-        draggable.makeDraggable(submarine2);
+    public void handleDragDropped(DragEvent event) { //evento para arrastrar y solta
+        tableroAliado.handleDragDropped(event);
     }
-
-
-
-
 
     public void setPlayer(Player player) {
         this.player = player;
@@ -545,9 +70,6 @@ public class GameController {
     }
 
     public void setNicknameToLabel(String nickname) {
-         this.nicknameLabel.setText("Welcome " + nickname +"!");
+        this.nicknameLabel.setText("Welcome " + nickname + "!");
     }
 }
-
-
-
